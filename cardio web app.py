e66731b86f17b8ec90e0f6815d66e2b581a9fde8
Yousef Mohamed <155397672+YouseFouly@users.noreply.github.com>
2026-09-03 19:49:06 +0300

    Update cardio web app.py

--- cardio web app.py
+++ cardio web app.py
@@ -9,10 +9,10 @@ import pickle
 import streamlit as st 
 from streamlit_option_menu import option_menu 
 
-diabetes_model = pickle.load(open('C:/Users/hp/OneDrive/Desktop/Turkey/diabetes_model.sav', 'rb'))
-heart_failure_model = pickle.load(open('C:/Users/hp/OneDrive/Desktop/Turkey/heart failure model.sav', 'rb'))
-obesity_model = pickle.load(open('C:/Users/hp/OneDrive/Desktop/Turkey/obesity_model.sav', 'rb'))
-storkes_model = pickle.load(open('C:/Users/hp/OneDrive/Desktop/Turkey/strokes_model.sav', 'rb'))
+diabetes_model = pickle.load(open('diabetes_model.sav', 'rb'))
+heart_failure_model = pickle.load(open('heart failure model.sav', 'rb'))
+obesity_model = pickle.load(open('obesity_model.sav', 'rb'))
+storkes_model = pickle.load(open('strokes_model.sav', 'rb'))
 
     
     
